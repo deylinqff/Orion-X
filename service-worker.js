89d14266c888bc88e44a110e17d423ecb4f644ab
deylinqff <maycraftminecraft4@gmail.com>
2025-04-29 17:26:32 -0600

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,16 @@
-// service-worker.js
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("fetch", (e) => {});
+self.addEventListener('install', function (event) {
+  console.log('[Service Worker] Instalado');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+  console.log('[Service Worker] Activado');
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
